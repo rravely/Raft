985d74cae54360480d8bac8b686502932071f0e9
rahyun <rravely@sookmyung.ac.kr>
2023-07-07 17:36:39 +0900

    [Create] Fish item database

--- Assets/3.Script/Item/FishItemDatabase.cs
+++ Assets/3.Script/Item/FishItemDatabase.cs
@@ -2,7 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class FishingInfo
+{
+    public Item item;
+    public float thirst;
+    public float hungry;
+
+    public FishingInfo(Item item, float thirst, float hungry)
+    {
+        this.item = item;
+        this.thirst = thirst;
+        this.hungry = hungry;
+    }
+}
+
 public class FishItemDatabase : MonoBehaviour
 {
-    
+    public static FishItemDatabase instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    [Header("HerringRaw")]
+    public Item herringRaw;
+    public float herringRawT;
+    public float herringRawH;
+
+    [Header("HerringCooked")]
+    public Item herringCooked;
+    public float herringCookedT;
+    public float herringCookedH;
+
+    [Header("PomfretRaw")]
+    public Item pomfretRaw;
+    public float pomfretRawT;
+    public float pomfretRawH;
+
+    [Header("PomfretCooked")]
+    public Item pomfretCooked;
+    public float pomfretCookedT;
+    public float pomfretCookedH;
+
+    [Header("MackerelRaw")]
+    public Item MackerelRaw;
+    public float MackerelRawT;
+    public float MackerelRawH;
+
+    [Header("MackerelCooked")]
+    public Item MackerelCooked;
+    public float MackerelCookedT;
+    public float MackerelCookedH;
+
+    [Header("TilapiaRaw")]
+    public Item TilapiaRaw;
+    public float TilapiaRawT;
+    public float TilapiaRawH;
+
+    [Header("TilapiaCooked")]
+    public Item TilapiaCooked;
+    public float TilapiaCookedT;
+    public float TilapiaCookedH;
 }
