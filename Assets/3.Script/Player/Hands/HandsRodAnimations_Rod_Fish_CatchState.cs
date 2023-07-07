4f216643ca01c63d6911f948764979996a9941ac
rahyun <rravely@sookmyung.ac.kr>
2023-07-07 17:26:30 +0900

    [Feat] Fishing

--- Assets/3.Script/Player/Hands/HandsRodAnimations_Rod_Fish_CatchState.cs
+++ Assets/3.Script/Player/Hands/HandsRodAnimations_Rod_Fish_CatchState.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class HandsRodAnimations_Rod_Fish_CatchState : StateMachineBehaviour
 {
+    HandsRod handsRod;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        handsRod = animator.GetComponent<HandsRod>();
+    }
+
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.SetBool("RodBack", false);
-
         //Add Item
+        handsRod.AcquiredRandomFish();
     }
 }
